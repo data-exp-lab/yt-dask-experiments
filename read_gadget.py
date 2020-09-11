f734e103dcd74fbebbf8f9beac8d30bb7f621501
Matthew Turk <matthewturk@gmail.com>
2020-09-11 12:38:32 -0500

    Implement low-level IO with mocks

--- read_gadget.py
+++ read_gadget.py
@@ -3,3 +3,22 @@ import yt
 ds = yt.load_sample("snapshot_033")
 
 reg = ds.all_data()
+
+class MockSelector:
+    is_all_data = True
+
+class MockChunkObject:
+    def __init__(self, data_file):
+        self.data_files = [data_file]
+
+class MockChunk:
+    def __init__(self, data_file):
+        self.objs = [MockChunkObject(data_file)]
+
+ptf = {'PartType0': ['Coordinates']}
+
+chunks = [MockChunk(data_file) for data_file in ds.index.data_files]
+selector= MockSelector()
+
+my_gen = ds.index.io._read_particle_fields(chunks, ptf, selector)
+my_result = [_ for _ in my_gen]
